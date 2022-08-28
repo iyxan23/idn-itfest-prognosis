fc7baf010231426c53527c219f560116d6cbde13
Iyxan23 <nurihsanalghifari@gmail.com>
2022-08-28 10:56:42 +0700

    feat: finally about-us!

---
+++ about-us.js
@@ -0,0 +1,12 @@
+
+const header = document.getElementById("header");
+const main = document.getElementById("main");
+
+function updateSizes() {
+    const headerRect = header.getBoundingClientRect();
+
+    main.style.paddingTop = `calc(${headerRect.height}px + 20vh)`;
+}
+
+onresize = updateSizes;
+updateSizes();
