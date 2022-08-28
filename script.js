dbe3ef3aad66b6224b380081e17e1c0d586353a2
Iyxan23 <nurihsanalghifari@gmail.com>
2022-08-28 09:35:10 +0700

    feat: design carousel content stuff

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ const main = document.getElementById("main");
 const carousel = document.getElementById("carousel");
 const bottomButtons = document.getElementById("bottom-buttons");
 
-const headerRect = header.getBoundingClientRect();
-const bottomButtonsRect = bottomButtons.getBoundingClientRect();
+function updateSizes() {
+    const headerRect = header.getBoundingClientRect();
+    const bottomButtonsRect = bottomButtons.getBoundingClientRect();
 
-main.style.paddingTop = `calc(${headerRect.height}px + 3rem)`;
-carousel.style.height = `calc(100vh - ${headerRect.height}px - ${bottomButtonsRect.height}px - 6.5rem)`
+    main.style.paddingTop = `calc(${headerRect.height}px + 3rem)`;
+    carousel.style.height = `calc(100vh - ${headerRect.height}px - ${bottomButtonsRect.height}px - 6.5rem)`
+}
+
+onresize = updateSizes;
+updateSizes();
