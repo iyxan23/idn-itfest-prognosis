e6567902ab0c86621800977f883cf11c794c1528
Iyxan23 <nurihsanalghifari@gmail.com>
2022-09-10 20:47:03 +0700

    feat: add a left and right arrow to the carousel

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const header = document.getElementById("header");
 const main = document.getElementById("main");
 const carousel = document.getElementById("carousel");
+const carouselCards = document.getElementById("carousel-cards");
 const bottomButtons = document.getElementById("bottom-buttons");
+
+var cardPositions = Array.from(
+            document.querySelectorAll("#carousel > #carousel-cards > .carousel-card").values()
+        ).map((i) => i.offsetLeft);
+
+var currentCardIndex = 0;
 
 function updateSizes() {
     const headerRect = header.getBoundingClientRect();
@@ -13,6 +20,29 @@ function updateSizes() {
         carousel.style.height = `calc(100vh - ${headerRect.height}px - 5rem)`
     } else {
         carousel.style.height = `calc(100vh - ${headerRect.height}px - ${bottomButtonsRect.height}px - 6.5rem)`
+    }
+
+    cardPositions = Array.from(document.querySelectorAll("#carousel > #carousel-cards > .carousel-card").values()).map((i) => i.offsetLeft);
+}
+
+carouselCards.onscroll = () => {
+    currentCardIndex = cardPositions.indexOf(
+        cardPositions.reduce(
+            (prev, curr) => Math.abs(curr - carouselCards.scrollLeft) < Math.abs(prev - carouselCards.scrollLeft) ? curr : prev)
+    );
+};
+
+function carouselLeft() {
+    if (currentCardIndex != 0) {
+        currentCardIndex -= 1;
+        carouselCards.scrollTo(cardPositions[currentCardIndex], 0);
+    }
+}
+
+function carouselRight() {
+    if (currentCardIndex < cardPositions.length) {
+        currentCardIndex += 1;
+        carouselCards.scrollTo(cardPositions[currentCardIndex], 0);
     }
 }
 
