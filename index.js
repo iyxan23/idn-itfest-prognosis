91f3f5222d3a769a41bedadb048f3759cb030134
Iyxan23 <nurihsanalghifari@gmail.com>
2022-09-14 21:19:49 +0700

    feat: create a fancy carousel autoscroll

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const main = document.getElementById("main");
 const carousel = document.getElementById("carousel");
 const carouselCards = document.getElementById("carousel-cards");
 const bottomButtons = document.getElementById("bottom-buttons");
+const carouselBar = document.getElementById("bar");
+
+const carouselIntervalDelay = 10000;
+var carouselInterval = setInterval(carouselRight, carouselIntervalDelay);
 
 var cardPositions = Array.from(
             document.querySelectorAll("#carousel > #carousel-cards > .carousel-card").values()
@@ -43,6 +47,34 @@ function carouselRight() {
     if (currentCardIndex < cardPositions.length) {
         currentCardIndex += 1;
         carouselCards.scrollTo(cardPositions[currentCardIndex], 0);
+    }
+}
+
+var autoScrolling = true;
+var interactionTimeout;
+function notifyAutoScroll() {
+    if (autoScrolling) {
+        autoScrolling = false; // disable the autoscroll
+        clearInterval(carouselInterval);
+
+        carouselBar.style.animation = "none";
+        carouselBar.style.width = 0;
+        carouselBar.offsetHeight; // trigger reflow
+
+        if (interactionTimeout) {
+            clearTimeout(interactionTimeout);
+            interactionTimeout = null;
+        }
+
+        // wait for 10s if there hasn't been any user interaction
+        interactionTimeout = setTimeout(() => {
+            // we can start again
+            autoScrolling = true;
+            carouselInterval = setInterval(carouselRight, carouselIntervalDelay);
+
+            carouselBar.style.animation = "bar-progress var(--carousel-interval) infinite linear";
+            carouselBar.offsetHeight; // trigger reflow
+        }, 10000);
     }
 }
 
